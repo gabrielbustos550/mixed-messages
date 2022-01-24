8f9331a8aa281f739f6481fb1385b6653623e2f9
Gabe Bustos <bustosfloresgabriel@gmail.com>
2022-01-24 14:32:35 -0700

    saved message components

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,1 +1,8 @@
-console.log('Hello world')
+
+const messages = {
+    introduction: [ 'You should: ', 'Never', 'You could: '], 
+    fortune: ['purchase a lottery ticket', 'travel internationally', 'eat cheese' ],
+    conclusion: ['!', '.', '!!!!']
+}
+
+console.log(messages)
