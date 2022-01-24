2e7200bfd7b0a2e260770f1eda93f19a14a5d3d9
Gabe Bustos <bustosfloresgabriel@gmail.com>
2022-01-24 15:51:46 -0700

    finished product

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,18 +1,20 @@
-
+// where different fortune options will be stored
 const messages = {
-    introduction: [ 'You should: ', 'Never', 'You could: '], 
+    introduction: [ 'You should: ', 'Never ', 'You could: '], 
     fortune: ['purchase a lottery ticket', 'travel internationally', 'eat cheese' ],
     conclusion: ['!', '.', '!!!!']
 }
-
+// create a random number generator to pull differnt messages 
 const randomIndex = () => {
    let randomNumber = Math.floor(Math.random() * 3)
    return randomNumber
 }
 
-console.log(randomIndex())
+const fortune = () => {
+    let sentence = messages.introduction[randomIndex()] + messages.fortune[randomIndex()] + messages.conclusion[randomIndex()]
+    console.log(sentence)
 
-const fortuneTeller = (obj) => {
-    console.log('Fortune teller')
+
 }
 
+fortune();
